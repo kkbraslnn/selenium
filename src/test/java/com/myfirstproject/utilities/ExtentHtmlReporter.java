431b9070b981b9adabe92fd8afcc8ee022733a3a
kkbraslnn <kkaratash@icloud.com>
2023-05-19 20:13:01 +0200

    v7

---
+++ src/test/java/com/myfirstproject/utilities/ExtentHtmlReporter.java
@@ -0,0 +1,13 @@
+package com.myfirstproject.utilities;
+
+import com.aventstack.extentreports.reporter.configuration.FileReporterConfig;
+
+public class ExtentHtmlReporter {
+    public ExtentHtmlReporter(String path) {
+    }
+
+    public FileReporterConfig config() {
+
+        return null;
+    }
+}
